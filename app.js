6d04b9d566df33f8fc718133a0d048e25be588f7
manalimatkar <manali.matkar@gmail.com>
2016-11-22 11:27:13 -0600

    async call problem in article calls

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+// We are using new api for getting news content from different sources and different categories
+// Include new api
 var NewsApi = require('news-api-njs'),
     config = require('./config/config.json');
     console.log(config);
-
+// authentication
 var news = new NewsApi({
     apiKey: config.apiKey
 });
 
+// define global arrays
 var sourceArr = [];
 
 var articlesArry = [];
+
 // this code will cover search for top stories from 
-// multiple sources when user selects the category from grid layout
+// multiple sources when user selects the category ui for now its hardcoded
 news.getSources({
 	category: 'business', //replace category with value user selects
 	language: 'en',
@@ -18,27 +22,25 @@ news.getSources({
 }).then(function(res) {
 	// console.log(res.sources.length);
 	var category = res.sources[0].category;
+	// for number of resources present create a array of source object.
 	for (var i = 0; i < res.sources.length; i++) {
 		var categorysource = {
 			categoryName : category,
-			sourceId: res.sources[i].id,
+			sourceName: res.sources[i].id,
 			sortBy: res.sources[i].sortBysAvailable[0],
-			sourceDetails: {
-				name: res.sources[i].name,
-				description: res.sources[i].description,
-				url: res.sources[i].url,
-				language: res.sources[i].language,
-				country: res.sources[i].country,
-				logoUrl: res.sources[i].urlsToLogos
-			}
+			sourceDetails : res.sources[i]
 		}
-
+		//push categorysource to array
 		sourceArr.push(categorysource);	
 	}
 
 	console.log(sourceArr);	
-
+	// call function to fetch articles from each source 
 	fetchArticleData();
+
+	// PROBLEM IS HERE AFTER I PUSH THE ARTICLES RESULT TO ARRAY 
+	//ITS NOT AVAILABLE EXCEPT INSIDE THE FOR LOOP INSIDETHE ABOVE FUNCITION
+	console.log(articlesArry);
 
 }).catch(function(err) {
 	console.log(err);
@@ -48,35 +50,24 @@ news.getSources({
 
 var fetchArticleData = function(){
 	for (var j = 0; j < sourceArr.length ; j++) {
-		var sourceName = sourceArr[j].sourceId;
-		var sortByOpt = sourceArr[j].sortBy;
-		var category = sourceArr[j].categoryName;
-		var sourceInfo = sourceArr[j].sourceDetails;
-		console.log("inside fetchArticleData For Loop " + sourceName);
-		news.getArticles({
-			source: sourceName,
-			sortBy: sortByOpt
-		}).then(function(result) {
-			// console.log(result);
-			// console.log("=============================================================================")
-			for (var x = 0; x < result.articles.length; x++) {
-				var articleObj = {
-					sourceName: sourceName,
-					sortByOpt: sortByOpt,
-					category: category,
-					article: result.articles[x],
-					source: sourceInfo						
-				}
 
-				articlesArry.push(articleObj);
-			}
-			console.log(articlesArry);
+		news.getArticles({
+			source: sourceArr[j].sourceName,
+			sortBy: sourceArr[j].sortBy
+		}).then(function(result) {
+			articlesArry.push(result);	
+			console.log("=========================================================");
+			console.log(articlesArry);	
+			console.log("=========================================================");			
+				
 		}).catch(function(err) {
 			console.log(err);
-		});
-
+		});	
 	}
-
+    // ARTICLESARRY IS EMPTY HERE
+	console.log("=========================================================");
+	console.log(articlesArry);	
+	console.log("=========================================================");		
 }
 
 
